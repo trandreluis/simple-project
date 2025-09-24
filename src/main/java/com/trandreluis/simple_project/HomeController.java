9b3bc1244ab77b377049bb79fe08120da55585f4
Andre Luis Gomes da Silva <tr.andreluis@gmail.com>
2025-09-23 21:30:31 -0300

    feat: home page with spring boot and thymeleaf

---
+++ src/main/java/com/trandreluis/simple_project/HomeController.java
@@ -0,0 +1,15 @@
+package com.trandreluis.simple_project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping({"/", "/home"})
+    public String home(Model model) {
+        model.addAttribute("message", "Hello, World!");
+        return "home";
+    }
+}
